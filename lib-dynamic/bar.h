c25d5c5d540928b29623700893e38f14d19ef5f6
sigman <sigman@ioupg.com>
2019-03-08 17:44:30 +0200

    Mention MSVC Warning  C4275

--- lib-dynamic/bar.h
+++ lib-dynamic/bar.h
@@ -6,6 +6,8 @@
 
 namespace bar {
 
+// inherit EXPORTED class from non EXPORTED results MSVC warning:
+// warning C4275: non dll-interface struct 'bar::detail' used as base for dll-interface struct 'bar::some'
 struct some: public detail {
   some();
   virtual ~some();
